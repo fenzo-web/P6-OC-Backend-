db958f72346f921c022dc3925c8e153bb9d8f28c
Enzo FLOCH <enzofloch37210@gmail.com>
2024-12-29 20:40:13 +0100

    Ajout books

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
+const bookRoutes = require("./routes/book");
+const path = require("path");
 
 const dbUser = process.env.USER;
 const dbPassword = process.env.PASSWORD;
@@ -28,8 +31,10 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
--- Backend/routes/book.js
+++ Backend/routes/book.js
@@ -2,5 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 const bookCtrl = require("../controllers/book");
+
+router.get("/", auth, bookCtrl.getAllBook);
+router.post("/", auth, multer, bookCtrl.createBook);
+router.get("/:id", auth, bookCtrl.getOneBook);
+router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.delete("/:id", auth, bookCtrl.deleteBook);
+//router.get("/bestrating", bookCtrl.getBestBook);
+//router.post("/:id/rating", bookCtrl.bookRating);
+
+module.exports = router;

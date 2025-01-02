24db866008f208345531fc02087bf93383ef174d
Enzo FLOCH <enzofloch37210@gmail.com>
2025-01-02 18:55:45 +0100

    user schema change

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-  email: String,
-  password: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
 const User = mongoose.model("User", UserSchema);

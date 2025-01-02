30636cde0dc146ae4ddcdd13e7b685bc071e21f9
Enzo FLOCH <enzofloch37210@gmail.com>
2025-01-02 19:52:13 +0100

    test sharp & changement modify book

--- Backend/controllers/book.js
+++ Backend/controllers/book.js
@@ -1,5 +1,6 @@
 const Book = require("../models/book");
 const fs = require("fs");
+const sharp = require("shrap");
 
 exports.getAllBook = (req, res, next) => {
   Book.find()
@@ -17,6 +18,21 @@ exports.createBook = (req, res, next) => {
   const bookObject = JSON.parse(req.body.book);
   delete bookObject._id;
   delete bookObject._userID;
+  // test sharp
+  if (!req.file) {
+    return res.status(400).json({ message: "Image not found" });
+  }
+
+  sharp(req.file.path)
+    .resize(500)
+    .toFormat("jpeg")
+    .jpeg({ quality: 80 })
+    .toFile(req.file.destination, "resized", req.file.filename)
+    .then(() => {
+      fs.unlinkSync(req.file.path);
+    })
+    .catch((error) => console.log(error));
+  // fin test sharp
   const book = new Book({
     ...bookObject,
     userId: req.auth.userId,
@@ -63,16 +79,30 @@ exports.modifyBook = (req, res, next) => {
     : { ...req.body };
 
   delete bookObject._userId;
+
   Book.findOne({ _id: req.params.id })
     .then((book) => {
       if (book.userId != req.auth.userId) {
         res.status(401).json({ message: "Not authorized" });
       } else {
+        if (req.body.author == "") {
+          return res.status(400).json({ message: "Author field empty " });
+        }
+        if (req.body.genre == "") {
+          return res.status(400).json({ message: "Genre field empty " });
+        }
+        if (req.body.title == "") {
+          return res.status(400).json({ message: "title field empty " });
+        }
+        if (req.body.year == "") {
+          return res.status(400).json({ message: "year field empty " });
+        }
+
         Book.updateOne(
           { _id: req.params.id },
           { ...bookObject, _id: req.params.id }
         )
-          .then(() => res.status(200).json({ message: "Book modifié!" }))
+          .then(() => res.status(200).json({ message: "Book modify !" }))
           .catch((error) => res.status(401).json({ error }));
       }
     })
